eb2c78faedb17d3512e84372f7f4205c9a9f03e2
Oleksii Sedun <oleksiisedun@gmail.com>
2026-07-29 10:26:10 +0300

    Reorder constants

--- Config.js
+++ Config.js
@@ -1,10 +1,12 @@
-const MAIN_SHEET_NAME = 'Main';
-const HEADER_LINE_MAX_LENGTH = 100;
-const CHUNK_TIME_BUDGET_MS = 1 * 60 * 1000;
+const POSITION_HEADER_REGEX = /^.*ПВ\s+[«"“].+[»"”].{0,10}$/;
+
 const GENERIC_POSITION_HEADER_REGEX = /[«"“].+[»"”]/;
 const BLOCK_END_REGEXES = [/ппд/i, /бпла/i];
 const IGNORE_LINE_REGEXES = [/заведено:/i, /виведено:/i];
-const POSITION_HEADER_REGEX = /^.*ПВ\s+[«"“].+[»"”].{0,10}$/;
+
+const MAIN_SHEET_NAME = 'Main';
+const HEADER_LINE_MAX_LENGTH = 100;
+const CHUNK_TIME_BUDGET_MS = 1 * 60 * 1000;
 
 const FOLDER_LINK_COLUMN = 'B2:B';
 const REPORT_COLUMN = 'C2:C';
